cf6a4f3969f5922c5b4c93342d45daeffd210fba
qizi <qizi0706@gmail.com>
2025-07-19 02:21:27 +0800

    Use maim to screenshot

--- config.def.h
+++ config.def.h
@@ -28,7 +28,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class            instance    title       tags mask     isfloating   monitor */
-	{ "Firefox",        NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "firefox",        NULL,       NULL,       1 << 1,       0,           -1 },
 	{ "mihomo-party",   NULL,       NULL,            0,       1,           -1 },
 };
 
@@ -59,10 +59,12 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "/home/celeb/.local/bin/dmenu-run-color", NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "kitty", NULL };
 static const char *volume_up[] = {"x-audio", "sink", "--plus"};
 static const char *volume_down[] = {"x-audio", "sink", "--minus"};
 static const char *slockcmd[] = { "slock", NULL };
+static const char *screenshot_cmd[] = { "sh", "-c", "maim -s ~/Pictures/$(date +%s).png", NULL };
+static const char *screenshot_clip[] = { "sh", "-c", "maim -s | xclip -selection clipboard -t image/png", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -71,6 +73,8 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_minus,  spawn,          {.v = volume_down } },
 	{ MODKEY,                       XK_equal,  spawn,          {.v = volume_up } },
 	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = slockcmd } },
+	{ MODKEY,                       XK_s,      spawn,          {.v = screenshot_cmd } },
+	{ MODKEY|ShiftMask,             XK_s,      spawn,          {.v = screenshot_clip } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
